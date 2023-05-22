9f59a5e56d8ac00e9e51a179bae90e79737ee1b3
Prasada Indukuri <prasada.indukuri_migration@specialized.com>
2023-05-22 20:45:33 +1000

    installed mui/material

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+
+class App extends React.Component {
+	constructor(props: any) {
+		super(props);
+		this.state = {};
+	}
+	
+	render() {
+		return (
+			<div className="app">
+				<div className="row">
+					<div className="mixed-chart">
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
