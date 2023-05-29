ece833a8c02114f72de6255ead3855c911aec779
Prasada Indukuri <prasada.indukuri_migration@specialized.com>
2023-05-29 20:32:12 +1000

    build fixes

--- src/modules/search-flights/components/FlightFinderFunctional/FlightFinderFunctional.tsx
+++ src/modules/search-flights/components/FlightFinderFunctional/FlightFinderFunctional.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
