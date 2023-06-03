79f971a5965c3c1f7c77eb673f1597b47db43070
Prasada Indukuri <prasada.indukuri_migration@specialized.com>
2023-06-04 01:14:10 +1000

    Buyer's Agency Application component that creates input components for user questions in a stepper

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,23 @@ import './App.scss';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Header from './modules/core/Header/Header';
-import Nav from './modules/core/Nav/Nav';
+import NavBar from './modules/core/NavBar/NavBar';
+
 import SearchFlightsPage from './modules/search-flights/SearchFlightsPage';
-import DataDrivenForm from './modules/data-driven-forms/DataDrivenForm';
+import DataDrivenPage from './modules/data-driven-forms/DataDrivenPage';
+import FormsInStepper from './modules/data-driven-forms/components/FormsInStepper/FormsInStepper';
 
 function App() {
 	return (
 		<div className="App">
 			<Router>
 				<Header />
-				<Nav />
+				<NavBar />
 
 				<Routes>
 					<Route path="/flight-finder" element={<SearchFlightsPage />} />
-					<Route path="/forms" element={<DataDrivenForm />} />
+					<Route path="/forms" element={<DataDrivenPage />} />
+					<Route path="/buyers-agency" element={<FormsInStepper />} />
 					<Route path="*" element={<h1>Page not found</h1>} />
 				</Routes>
 			</Router>
