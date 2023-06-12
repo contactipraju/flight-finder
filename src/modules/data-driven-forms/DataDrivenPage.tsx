55fd718d3b4ede0c4285ca5a4a9fce7c6e97cfc5
Prasada Indukuri <prasada.indukuri_migration@specialized.com>
2023-06-12 23:23:08 +1000

    Using ContextAPI to share state between different components (Admin page and Header).  Also loading context using local json file and updating it from Admin screen. Some cleanup done too

--- src/modules/data-driven-forms/DataDrivenPage.tsx
+++ src/modules/data-driven-forms/DataDrivenPage.tsx
@@ -1,49 +1,6 @@
 import './DataDrivenPage.scss';
 
-import { componentTypes } from '@data-driven-forms/react-form-renderer';
-
 import DataDrivenForm from './components/DataDrivenForm/DataDrivenForm';
-
-const schema = {
-	fields: [
-		{
-			component: componentTypes.TEXT_FIELD,
-			name: 'name',
-			label: 'Your name',
-			value: 'Prasada Indukuri',
-			isRequired: true,
-			validate: [{ type: 'required' }]
-		},
-		{
-			component: componentTypes.TEXT_FIELD,
-			name: 'number',
-			label: 'Your contact number',
-			value: '+61 45 8866 586',
-			isRequired: true,
-			validate: [{ type: 'required' }]
-		},
-		{
-			component: componentTypes.TEXT_FIELD,
-			name: 'email',
-			label: 'Your email id',
-			value: 'contactipraju@gmail.com',
-			isRequired: true,
-			validate: [{ type: 'required' }]
-		},
-		{
-			component: componentTypes.TEXT_FIELD,
-			name: 'linkedin',
-			label: 'LinkedIn Url',
-			value: 'linkedin.com/in/prasadaraju'
-		},
-		{
-			component: componentTypes.TEXT_FIELD,
-			name: 'github',
-			label: 'GitHub Url',
-			value: 'https://github.com/contactipraju'
-		}
-	]
-}
 
 const DataDrivenPage = () => {
 	return (
@@ -52,7 +9,7 @@ const DataDrivenPage = () => {
 			<div className="desc">  </div>
 
 			<div className="content">
-				<DataDrivenForm data={schema} />
+				<DataDrivenForm />
 			</div>
 		</div>
 	)
