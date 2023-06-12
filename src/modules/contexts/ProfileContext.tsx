bba1613533d87c4464d69c24117c639567f48da6
Prasada Indukuri <prasada.indukuri_migration@specialized.com>
2023-06-12 23:28:59 +1000

    eslint fix

--- src/modules/contexts/ProfileContext.tsx
+++ src/modules/contexts/ProfileContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react";
+import { createContext } from "react";
 import { useState, useEffect } from "react";
 
 import { getProfile }  from './../core/Profile/Profile.service';
